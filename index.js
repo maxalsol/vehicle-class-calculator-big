15b6a27c3cc8f6baacc6dd78a897ce761c9d5965
maxalsol <max.pallat@gmail.com>
2023-09-04 19:34:15 +1200

    nav bar button working but ugly looking

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const mobileMenuButton = document.getElementById("mobile-menu");
 const navList = document.querySelector(".nav-list");
 
 mobileMenuButton.addEventListener("click", () => {
-    navList.classList.toggle("active");
+    navList.classList.toggle("nav-toggle");
 });
