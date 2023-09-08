61339e6c4be35dc4ae9995b74df6f028a75afac9
maxalsol <max.pallat@gmail.com>
2023-09-09 01:01:08 +1200

    added dropdown for account to nav and edited media ueries for logo

--- loggedIn/index.js
+++ loggedIn/index.js
@@ -1,6 +1,10 @@
 const mobileMenuButton = document.getElementById("mobile-menu");
 const navList = document.querySelector(".nav-list");
-
 mobileMenuButton.addEventListener("click", () => {
     navList.classList.toggle("nav-toggle");
+});
+
+const accountButton = document.getElementById("acc-btn");
+accountButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
 });
